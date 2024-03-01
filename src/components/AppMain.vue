4df0e132b516767ba8c0769abe8344186226464a
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-03-01 16:43:35 +0100

    End Milestone 4 with changes on laravel-api

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,23 @@
 <script>
+import { store } from '../store.js';
+import axios from 'axios';
+
 export default {
     name: 'AppMain',
     data() {
         return {
+            store,
             projects: [],
+        }
+    },
+    created() {
+        this.getProject();
+    },
+    methods: {
+        getProject() {
+            axios.get(`${this.store.baseUrl}/api/projects`).then((response) => {
+                console.log(response.data.results);
+            })
         }
     }
 }
