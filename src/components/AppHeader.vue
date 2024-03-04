19280407341dfb7fb343515948e69724459a8a47
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-03-04 16:20:27 +0100

    Created header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,9 +5,25 @@ export default {
 </script>
 
 <template lang="">
-    <div>
-        
-    </div>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="d-flex justify-content-between align-items-center my-3">
+                    <h2>
+                        Boolfolio
+                    </h2>
+                    <ul class="list-unstyled d-flex">
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Progetti</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
 </template>
 
 <style lang="">
