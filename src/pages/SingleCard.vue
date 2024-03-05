3b4abfc264f1375640d18ce2f78fb0a6f1438718
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-03-05 16:16:29 +0100

    Added method to get singlecard

--- src/pages/SingleCard.vue
+++ src/pages/SingleCard.vue
@@ -1,21 +1,30 @@
 <script>
+import axios from 'axios';
 import { store } from '../store';
 export default {
     name: 'Singlecard',
     data(){
         return{
             store,
+            project: null,
         }
     },
+    created(){
+        this.getSingleProject();
+    },
     methods:{
-
+        getSingleProject(){
+            axios.get(`${this.store.baseUrl}/api/projects/${this.$route.params.slug}`).then((response)=>{
+                this.project = response.data.project
+            });
+        }
     }
 }
 </script>
 
 <template lang="">
     <div>
-        <h2>ciao</h2>
+        <h2>{{project.name}}</h2>
     </div>
 </template>
 
