c718042b03f22fb3c772509235041001f2ad5630
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-03-10 14:50:06 +0100

    Created AppContact pages

---
+++ src/pages/AppContactUs.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'AppContactUs'
+}
+</script>
+
+<template lang="">
+    <div class="container">
+        <div class="row">
+            <h1>Contatti</h1>
+        </div>
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
